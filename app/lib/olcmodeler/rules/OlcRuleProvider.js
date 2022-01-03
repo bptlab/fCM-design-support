a7a62ac7c29c9eaa57370b5c3ca80ebde2cdb475
Leon Bein <leon.bein@student.hpi.de>
2022-01-03 16:06:06 +0100

    Forbid multiedges
    (but leadsto reverse edges)

--- app/lib/olcmodeler/rules/OlcRuleProvider.js
+++ app/lib/olcmodeler/rules/OlcRuleProvider.js
@@ -4,22 +4,33 @@ import RuleProvider from 'diagram-js/lib/features/rules/RuleProvider';
 
 import {is} from '../../util/Util';
 
-export default function OlcRuleProvider(eventBus) {
+export default function OlcRuleProvider(eventBus, elementRegistry) {
+  this._elementRegistry = elementRegistry;
   RuleProvider.call(this, eventBus);
 }
 
-OlcRuleProvider.$inject = ['eventBus'];
+OlcRuleProvider.$inject = [
+  'eventBus', 
+  'elementRegistry'
+];
 
 inherits(OlcRuleProvider, RuleProvider);
 
 
 OlcRuleProvider.prototype.init = function () {
 
-  this.addRule('connection.create', function (context) {
-    var source = context.source,
-      target = context.target;
+  var self = this;
 
-    return is(source, 'olc:State') && is(target, 'olc:State') && { type: 'olc:Transition' };
+  this.addRule('connection.create', function (context) {
+    var {source, target} = context;
+    
+    // There must not be more than one transition between two states
+    var existingConnections = self._elementRegistry.filter(function(element) {
+      return is(element, 'olc:Transition') && element.source === source && element.target === target;
+    });
+    //TODO this leads to reverse connections being created because of diagram-js' Connect.js trying to
+
+    return is(source, 'olc:State') && is(target, 'olc:State') && existingConnections.length === 0 && { type: 'olc:Transition' };
   });
 
   this.addRule('connection.start', function (context) {
