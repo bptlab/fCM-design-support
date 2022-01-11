a425232b8cf789dc8c3912a4e54d1b35f9cb190c
Marc <marc.rosenau@student.hpi.de>
2022-01-11 15:23:28 +0100

    Implement resize on error bar

--- app/index.js
+++ app/index.js
@@ -171,7 +171,6 @@ document.getElementById('importOlc').addEventListener('click', function() {
 
 //Make the DIV element draggagle:
 dragElement(document.getElementById("note-area-wrapper"));
-
 function dragElement(elmnt) {
       
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -227,4 +226,50 @@ function toggleNoteArea() {
 
 
 // function to make the error bar expand
+
+resizeErrorBar(document.getElementById("errorBar"));
+
+function resizeErrorBar(elmnt) {
+    
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    
+    var mainContent = document.getElementById("main");
+    
+    elmnt.onmousedown = dragMouseDown;
+    
+    function dragMouseDown(e) {
+
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        //pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+  function elementDrag(e) {
+    
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        //pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        //pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        //elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.height = elmnt.offsetHeight + pos2 + "px";
+        mainContent.style.height = mainContent.offsetHeight - pos2 + "px";
+  }
+
+  function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+  }
+    
+    
+}
 
