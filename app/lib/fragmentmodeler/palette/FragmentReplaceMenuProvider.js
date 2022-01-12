9c8179fa8ae16dfd25a12328b12fd65cc8617ca6
MaximilianKoenig <koenigmax1@web.de>
2022-01-12 16:42:28 +0100

    🐛 [Fragments] Remove more illegal elements

--- app/lib/fragmentmodeler/palette/FragmentReplaceMenuProvider.js
+++ app/lib/fragmentmodeler/palette/FragmentReplaceMenuProvider.js
@@ -15,6 +15,23 @@ FragmentReplaceMenuProvider.prototype.getPopupMenuEntries = function(element) {
             delete entries['replace-with-escalation-intermediate-throw'];
             delete entries['replace-with-link-intermediate-throw'];
             delete entries['replace-with-link-intermediate-catch'];
+            delete entries['replace-with-compensation-intermediate-throw'];
+        }
+
+        if (is(element, 'bpmn:Task')) {
+            delete entries['replace-with-expanded-subprocess'];
+            delete entries['replace-with-collapsed-subprocess'];
+            delete entries['replace-with-call-activity'];
+        }
+
+        if (is(element, 'bpmn:Gateway')) {
+            delete entries['replace-with-complex-gateway'];
+            delete entries['replace-with-inclusive-gateway'];
+            delete entries['replace-with-parallel-gateway'];
+        }
+
+        if (is(element, 'bpmn:DataObjectReference')) {
+            delete entries['replace-with-data-store-reference'];
         }
 
         return entries;
