4dc372cbd339ab848e424b7437d2db6aefbd932f
Leon Bein <leon.bein@student.hpi.de>
2022-02-03 12:39:03 +0100

    ✨ [GL] Add (un-)hide button to error area #37

--- app/lib/guidelines/ErrorBar.js
+++ app/lib/guidelines/ErrorBar.js
@@ -1,7 +1,12 @@
 export default class ErrorBar {
     constructor(element) {
         this.element = element;
-        this.table = element.getElementsByTagName('table')[0];
+        makeResizable(element);
+        this.table = document.getElementById('errorTable');
+        this.toggleTableButton = document.getElementById('toggleErrorTable');
+        this.toggleTableButton.addEventListener('click', event => {
+            this.toggleTable();
+        });
     }
 
     clear() {
@@ -10,7 +15,7 @@ export default class ErrorBar {
         }
     }
 
-    displayRow({severity, element, artifact, message, link, quickFixes}) {
+    displayRow({ severity, element, artifact, message, link, quickFixes }) {
         const row = this.table.insertRow(-1);
         row.classList.add(severity.cssClass);
         row.classList.add('violationRow');
@@ -29,5 +34,55 @@ export default class ErrorBar {
             quickFixesCell.appendChild(quickFixesButton);
             //TODO make functional
         }
+    }
+
+    toggleTable() {
+        this.element.classList.toggle('hidingTable');
+    }
+
+}
+
+
+
+function makeResizable(elmnt) {
+
+    // TODO now we have two kinds of code for resizing: dividers and this here
+
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    elmnt.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+
+        // get the mouse cursor position at startup:
+        //pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        if (elmnt.classList.contains('hidingTable')) return;
+
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        //pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        //pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        //elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.height = elmnt.offsetHeight + pos2 + "px";
+        //mainContent.style.height = mainContent.offsetHeight - pos2 + "px";
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
     }
 }
