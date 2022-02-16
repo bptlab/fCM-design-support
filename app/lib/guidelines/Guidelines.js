f2439bb78f1e604541fd7d9e57a067979acf44f3
Leon Bein <leon.bein@student.hpi.de>
2022-02-16 10:29:36 +0100

    🚑 Hotifx c5

--- app/lib/guidelines/Guidelines.js
+++ app/lib/guidelines/Guidelines.js
@@ -305,7 +305,9 @@ export default [
                 classDependencies[dependentClass.id].push(contextClass);
             }
             const associations = dataModeler.get('elementRegistry').filter(element => is(element, 'od:Association') && element.type !== 'label').map(association => association.businessObject);
-            associations.forEach(association => {
+            associations
+            .filter(association => association.sourceCardinality && association.targetCardinality) // TODO this is an hotfix
+            .forEach(association => {
                 const [sourceLowerBound, sourceUpperBound] = association.sourceCardinality.split('..');
                 const [targetLowerBound, targetUpperBound] = association.targetCardinality.split('..');
                 if (parseInt(sourceLowerBound) > 0) {
